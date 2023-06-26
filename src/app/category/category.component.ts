201b0b4570311b753ce8f84e1e00df7e0f91f4ad
Lakshmanan <lakshmanvellingri@gmail.com>
2023-06-26 21:31:59 +0530

    isEnable

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -7,12 +7,18 @@ import { Component } from '@angular/core';
     styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent {
+
+  isEnable: boolean = false
     
    constructor() {
     
    }
 
     ngOnInit(): void {
+        if(this.isEnable == true)
+         {
+            console.log(this.isEnable)
+         }
        
     }
 
